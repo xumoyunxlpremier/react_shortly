09740c0e2d99afb4ff7b78e18cd48ce59a5a27b9
Xumoyun <Xumoyun>
2025-10-24 15:53:49 +0500

    finish

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,51 @@
-import style from "./header.module.css"
+import { useState } from "react";
+import style from "./header.module.css";
 
 export default function Header() {
-    return <div>
-        {/* <!-- Header --> */}
+    
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    
+    const toggleMenu = () => {
+        setMenuOpen((prev) => !prev);
+    };
+
+    return (
         <header className={style.header}>
             <div className={style.container}>
                 <div className={style.header__container}>
                     <div className={style.header__containertop}>
-                        <div className={style.logo} >
+                        {/* Logo */}
+                        <div className={style.logo}>
                             <a href="#" className={style.logolink}>Shortly</a>
                         </div>
 
-                        {/* <!--  Navigation --> */}
+                        {/* Navigation */}
                         <nav className={style.nav}>
-                            <ul className={style.navlinks}>
-                                <li ><a href="#" >Features</a></li>
-                                <li ><a href="#">Pricing</a></li>
-                                <li ><a href="#">Resources</a></li>
+                            <ul
+                                className={`${style.navlinks} ${menuOpen ? style.active : ""}`}
+                            >
+                                <li><a href="#">Features</a></li>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="#">Resources</a></li>
                             </ul>
                         </nav>
                     </div>
 
-                    {/* <!-- Desktop Registration --> */}
-                    <div className={style.registration}>
-                        <a href="#"  className={style.login}>Login</a>
-                        <a href="#"  className={style.sign}>Sign Up</a>
+                    {/* Registration */}
+                    <div
+                        className={`${style.registration} ${menuOpen ? style.active : ""}`}
+                    >
+                        <a href="#" className={style.login}>Login</a>
+                        <a href="#" className={style.sign}>Sign Up</a>
                     </div>
 
-                    {/* <!-- Mobile Menu Button --> */}
-                    <button className={style.hamburger} aria-label="Toggle menu">
+                    {/* Hamburger */}
+                    <button
+                        className={`${style.hamburger} ${menuOpen ? style.active : ""}`}
+                        onClick={toggleMenu}
+                        aria-label="Toggle menu"
+                    >
                         <span></span>
                         <span></span>
                         <span></span>
@@ -36,5 +53,6 @@ export default function Header() {
                 </div>
             </div>
         </header>
-    </div>;
+    );
 }
+2
